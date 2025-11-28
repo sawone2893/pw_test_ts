ee1835965c6917ccbad35d29e402307b3d7315e3
Shabbir Rayeen <sawone2893@gmail.com>
2025-11-29 01:50:34 +0530

    Updated import statement in test.spec.ts

--- tests/test.spec.ts
+++ tests/test.spec.ts
@@ -1,9 +1,12 @@
-import {test} from '../utils/baseTest';
-import { decrypt } from '../src/utils/CryptoJSUtils';
-import {encryptEnvFile} from "../utils/EncryptEnvFile"
+import { test } from "../src/utils/baseTest";
+import { decrypt } from "../src/utils/CryptoJSUtils";
+//import { encryptEnvFile } from "../src/utils/EncryptEnvFile";
 
-test('Login Functionlity',async({pages})=>{
-await pages.homePage.goToHomePage();
-await pages.homePage.enterCredentials(decrypt(process.env.USER_ID!),decrypt(process.env.PASSWORD!));
+test("Login Functionlity", async ({ pages }) => {
+  //encryptEnvFile();
+  //await pages.homePage.goToHomePage();
+  await pages.homePage.enterCredentials(
+    decrypt(process.env.USER_ID!),
+    decrypt(process.env.PASSWORD!)
+  );
 });
-
